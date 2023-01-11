374c528c223eecfb37e7ad9ab3327d7b5dcae14d
John Seth Salazar <salazarjohnseth@gmail.com>
2023-01-11 08:35:57 +0800

    Header & Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,8 @@
-import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './MenuComponent';
 import {DISHES} from '../shared/dishes';
 import React, { Component } from 'react';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import DishDetail from './DishDetailComponent';
 
 class Main extends Component {
@@ -21,15 +22,12 @@ class Main extends Component {
   render(){
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className='container'>
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <Menu dishes={this.state.dishes}
             onClick={(dishId) => this.onDishSelect(dishId)}/>
         <DishDetail 
             dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+        <Footer />
       </div>
     );
   }
