78f911567a2ebca1a02ccb2e818d3114c8c2ea0a
johnsethsalazar <salazarjohnseth@gmail.com>
2023-01-19 21:22:28 +0800

    React Router and Single Page Applications Peer-graded Assignment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -37,6 +38,12 @@ class Main extends Component {
             );
         }
 
+        const AboutUsPage = () => {
+            return(
+                <About leaders={this.state.leaders} />
+            );
+        }
+
         const DishWithId = ({match}) => {
             return(
                 <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
@@ -53,6 +60,7 @@ class Main extends Component {
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path="/menu/:dishId" component={DishWithId}/>
                     <Route exact path="/contactus" component={Contact} />
+                    <Route exact path="/aboutus" component={AboutUsPage} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
